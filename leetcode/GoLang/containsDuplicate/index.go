1cdf9e45dda078dca6743e4e34071c24cfdec27c
tatazzi <otaviol.gon@gmail.com>
2024-03-01 15:09:48 -0300

    First commit and challenge contains duplicate

--- leetcode/GoLang/containsDuplicate/index.go
+++ leetcode/GoLang/containsDuplicate/index.go
@@ -14,3 +14,21 @@ func containsDuplicate(nums []int) bool {
 	}
 	return hasTwoOrMore
 }
+
+// Runtime: 85ms
+// Memory: 15.44mb
+
+//func containsDuplicate(nums []int) bool {
+//	sort.Slice(nums, func(i, j int) bool {
+//		return nums[i] > nums[j]
+//	})
+//	n := len(nums)
+//	for i := 0; i < n-1; i++ {
+//		if nums[i] == nums[i+1] {
+//			return true
+//		}
+//	}
+//	return false
+//}
+//Runtime: 90ms
+//memory: 8.12mb
